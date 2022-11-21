ad7432ec2e2a45ae9b90fb1875e04a57c9e978d7
Anders Abel <anders@abel.nu>
2022-11-21 13:11:35 +0100

    dotnet new isinmem

---
+++ v2/DuendeDynamicProviders/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DuendeDynamicProviders.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
